df0eb4b42a1b7c1732638504de04686dbf537b3d
Tomas Rivas <tomirivasacuna@gmail.com>
2022-09-03 20:40:45 -0400

    viernes

--- scene/players/player.gd
+++ scene/players/player.gd
@@ -1,11 +1,15 @@
-extends Node2D
-
+extends KinematicBody2D
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+export (int) var speedx =10
+export (int) var accelerationX =10
 
-var speed = 100
+var SPEED=100
+var accelerationY=0
+
+onready var sprite= $Pivot
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,6 +17,9 @@ func _ready():
 
 
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	var movementp1:Vector2 = Input.get_vector("Izquierda","Derecha","Arriba","Abajo")
+	move_and_slide(movementp1*SPEED)
+	
