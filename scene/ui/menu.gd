17d317f471254ce825eab88c91d77af1db104f4c
valeeBurgos <vale.burgos.sanhueza@gmail.com>
2022-09-08 23:35:18 -0400

    Se agregó cositas al menu, entre ellos funcionalidades básicas a los botones

---
+++ scene/ui/menu.gd
@@ -0,0 +1,20 @@
+extends MarginContainer
+
+onready var play = $VBoxContainer/Play
+onready var exit = $VBoxContainer/Exit
+onready var credits = $VBoxContainer/Credits
+
+func _ready():
+	play.connect("pressed", self, "_on_play_press")
+	credits.connect("pressed", self, "_on_credits_press")
+	exit.connect("pressed", self, "_on_exit_press")
+
+func _on_play_press():
+	get_tree(). change_scene("res://scene/players/player.tscn")
+
+
+func _on_credits_press():
+	get_tree(). change_scene("res://scene/credits.tscn")
+	
+func _on_exit_press():
+	get_tree().quit()
