858be4328bcc5dd742dae7e2d01ee279317a051d
Tomas Rivas <tomirivasacuna@gmail.com>
2022-09-10 23:50:58 -0300

    2 players controls and texture changer

--- scene/players/player.gd
+++ scene/players/player.gd
@@ -1,29 +1,41 @@
 extends KinematicBody2D
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 export (int) var speedx =10
 export (int) var accelerationX =10
-
+export var inputindex = 1
+export var texture=1
 var SPEED=200
-var accelerationY=0
 
 onready var sprite= $Pivot
+onready var sprite_text = $Pivot/Sprite
+
 onready var anim_player= $AnimationPlayer
 onready var anim_tree = $AnimationTree
 onready var playback = anim_tree.get("parameters/playback")
+onready var blue_dino = preload("res://texturas/personaje/sheets/DinoSprites - doux.png")
+onready var red_dino = preload("res://texturas/personaje/sheets/DinoSprites - mort.png")
+onready var yellow_dino = preload("res://texturas/personaje/sheets/DinoSprites - tard.png")
+onready var green_dino = preload("res://texturas/personaje/sheets/DinoSprites - vita.png")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	anim_tree.active=true
+	if  (texture == 1):
+		sprite_text.set_texture(blue_dino)
 
+	elif(texture == 2):
+		sprite_text.set_texture(red_dino)
 
+	elif(texture == 3):
+		sprite_text.set_texture(yellow_dino)
 
+	else:
+		sprite_text.set_texture(green_dino)
 
+		 
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	var movementp1:Vector2 = Input.get_vector("Izquierda","Derecha","Arriba","Abajo")
+	var movementp1:Vector2 = Input.get_vector("Izq"+str(inputindex),"Der"+str(inputindex),"Arriba"+str(inputindex),"Abajo"+str(inputindex))
 	move_and_slide(movementp1*SPEED)
 	if abs(movementp1.x)>0 or abs(movementp1.y)>0:
 		playback.travel("run")
@@ -31,5 +43,5 @@ func _physics_process(delta):
 		playback.travel("idle")
 	if movementp1.x<0:
 		sprite.set_scale(Vector2(-1,1))
-	else:
+	if movementp1.x>0:
 		sprite.set_scale(Vector2(1,1))
