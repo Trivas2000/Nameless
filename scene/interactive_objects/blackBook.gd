20c73e22fc9035c8710d7f96f9ff49d25d70fab5
valeeBurgos <vale.burgos.sanhueza@gmail.com>
2022-10-12 21:46:58 -0300

    Ahora pueden tomar objetos, pero no girar

--- scene/interactive_objects/blackBook.gd
+++ scene/interactive_objects/blackBook.gd
@@ -1,38 +1,4 @@
 extends RigidBody2D
 
 var picked=false;
-onready var player=get_node("../../players/player");
 #onready var pos_player = get_node("../../players/player/Position2D").global_position;
-
-
-func _physics_process(delta):
-	if picked == true:
-		self.position = self.position
-		#self.position = pos_player
-
-func _input(event):
-	if Input.is_action_just_pressed("ui_pick1"):
-		var bodies= $Area2D.get_overlapping_bodies()
-		for body in bodies:
-			if body.name=="player" and player.canPick==true:
-				picked=true;
-				player.canPick=false;
-				
-	if Input.is_action_just_pressed("ui_drop"):
-		picked=false
-		player.canPick=true
-		if player.sprite.flip_h == false:
-			apply_impulse(Vector2(), Vector2(90,0))
-		else:
-			apply_impulse(Vector2(), Vector2(90,0))
-			
-	if Input.is_action_just_pressed("ui_throw") and picked==false:
-		picked=false
-		player.canPick=true
-		if player.sprite.flip_h == false:
-			apply_impulse(Vector2(), Vector2(100,0))
-		else:
-			apply_impulse(Vector2(), Vector2(100,0))
-		
-		
-			
--- scene/players/player.gd
+++ scene/players/player.gd
@@ -4,7 +4,12 @@ export (int) var accelerationX =10
 export var inputindex = 1
 export var texture=1
 var SPEED=200
+
 var canPick = true;
+onready var pick_dino =$Pivot/Pick_dino
+var picked;
+
+var Black_book =preload("res://scene/interactive_objects/blackBook.tscn")
 
 onready var sprite= $Pivot
 onready var sprite_text = $Pivot/Sprite
@@ -32,9 +37,9 @@ func _ready():
 
 	else:
 		sprite_text.set_texture(green_dino)
-
-		 
-		
+	
+	
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	var movementp1:Vector2 = Input.get_vector("Izq"+str(inputindex),"Der"+str(inputindex),"Arriba"+str(inputindex),"Abajo"+str(inputindex))
@@ -48,3 +53,29 @@ func _physics_process(delta):
 	if movementp1.x>0:
 		sprite.set_scale(Vector2(1,1))
 		
+	if (Input.is_action_just_pressed("ui_pick"+str(inputindex))):	
+		_pick_object()			
+
+
+	
+func _pick_object(): 
+	if (canPick == false) :
+		self.remove_child(picked)
+		var black_book = Black_book.instance()
+		get_parent().add_child(black_book)
+		black_book.global_position =pick_dino.global_position
+		canPick=true
+	else :
+		for body in $Detector.get_overlapping_bodies():
+			if body.name=="blackBook" and canPick==true and body.picked == false:
+				canPick = false
+				body.picked = true
+				#forma 2
+				body.queue_free()
+				picked = Black_book.instance()
+				self.add_child(picked)
+				picked.picked = true
+				picked.global_position =pick_dino.global_position
+		
+
+				
