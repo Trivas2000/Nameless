9b24475004393e99220d3881c2a03371374e32e5
valeeBurgos <vale.burgos.sanhueza@gmail.com>
2022-12-12 15:25:27 -0300

    a

---
+++ scene/winArea/GameOver/Winner.gd
@@ -0,0 +1,30 @@
+extends MarginContainer
+ 
+onready var main_menu =$"%MainMenu"
+onready var exit = $"%Exit"
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready(): 
+	main_menu.connect("pressed",self, "on_main_menu_pressed")
+	exit.connect("pressed",self, "on_exit_pressed")
+	
+	hide() 
+	
+	
+func _input(event):
+	if event.is_action_pressed("menu"):
+		visible =! visible
+		get_tree().paused = visible 
+		
+	
+func on_main_menu_pressed():
+	get_tree().paused = false
+	get_tree().change_scene("res://scene/menu_scenes/menu.tscn")
+	
+func on_exit_pressed():
+	get_tree().quit()
+	
+	
