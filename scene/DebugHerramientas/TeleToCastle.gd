32d527370280a73b779c222b5c482a5041ef360d
YareliDanllely <90730049+YareliDanllely@users.noreply.github.com>
2022-12-12 16:31:55 -0300

    merge vale

---
+++ scene/DebugHerramientas/TeleToCastle.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _physics_process(delta):
+	pass
+			
+func use(player):
+	
+	player.position=Vector2(2495, 0)
--- scene/main.gd
+++ scene/main.gd
@@ -4,10 +4,10 @@ extends Node2D
 
 onready var camera1 = $Viewports/ViewportContainer1/Viewport1/Camera2D
 onready var camera2 = $Viewports/ViewportContainer2/Viewport2/Camera2D
-onready var map1 = $Viewports/ViewportContainer1/Viewport1/map1
+onready var map1 = $Viewports/ViewportContainer1/Viewport1/lvl2
 
 
 
 func _ready():
-	camera1.target = map1.get_node("TileMap3/player")
-	camera2.target = map1.get_node("TileMap3/player2")
+	camera1.target = map1.get_node("map1/TileMap3/player")
+	camera2.target = map1.get_node("map1/TileMap3/player2")
